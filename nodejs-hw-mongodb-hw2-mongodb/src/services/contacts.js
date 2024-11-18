b1a3195ad08cbb14a629d8bbcc993c60c0c44b18
RuslanAndr1107 <159262050+RuslanAndr1107@users.noreply.github.com>
2024-11-18 13:43:36 +0200

    Delete nodejs-hw-mongodb-hw2-mongodb directory

--- nodejs-hw-mongodb-hw2-mongodb/src/services/contacts.js
+++
@@ -1,5 +0,0 @@
-import contactCollection from '../db/models/contact.js';
-
-export const getAllContacts = () => contactCollection.find();
-
-export const getContactById = (id) => contactCollection.findById(id);
