5e5852d9335496793d7a663ed396baac322a506f
RuslanAndr1107 <androshulikruslan@gmail.com>
2024-11-18 17:07:21 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import { createFakeContact } from './utils/createFakeContact.js';
-console.log(createFakeContact());
+import { startServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  startServer();
+};
+
+bootstrap();
