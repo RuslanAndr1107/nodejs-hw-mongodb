745134d46e59fd1144b64c826500bfce07eb1199
RuslanAndr1107 <androshulikruslan@gmail.com>
2025-01-02 19:22:33 +0200

    4

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,15 +4,16 @@ import { env } from '../utils/env.js';
 export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
-    const password = env('MONGODB_PASSWORD');
-
-    const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
-    const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+    const password = env('MONGODB_PASSWORD'); 
+    const url = env('MONGODB_URL'); 
+    const db = env('MONGODB_DB'); 
+    const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
     await mongoose.connect(DB_HOST);
     console.log('Mongodb connection successfully');
   } catch (error) {
+
     console.log('Mongodb connection error', error.message);
     throw error;
   }
 };
+
