134aa1667a53cdb16e4a0dbd05e31051a19d5a93
RuslanAndr1107 <androshulikruslan@gmail.com>
2025-01-06 22:41:32 +0200

    2

--- src/server.js
+++ src/server.js
@@ -1,34 +1,35 @@
 import express from 'express';
-import pinoHttp from 'pino-http';
-import pino from 'pino';
+import pino from 'pino-http';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
   app.use(express.json());
-
-  const logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    transport: { target: 'pino-pretty' },
-  });
-
   app.use(cors());
-  app.use(pinoHttp({ logger }));
   app.use(cookieParser());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-  app.use('/', router); // Префікс для всіх маршрутів
-
-  app.use('*', notFoundHandler); // Обробка необроблених маршрутів
-
-  app.use(errorHandler); // Глобальний обробник помилок
+  app.use(router);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
-    logger.info(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT} `);
   });
 };
