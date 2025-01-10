423ab89aec124d2143411056a7bc4e299e03a069
RuslanAndr1107 <androshulikruslan@gmail.com>
2025-01-10 04:23:29 +0200

    FixresetPasswordEmail.html

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const FIFTEEN_MINUTES = 150 * 60 * 1000;
 export const THIHTY_DAY = 24 * 60 * 60 * 1000 * 30;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const SMTP = {
